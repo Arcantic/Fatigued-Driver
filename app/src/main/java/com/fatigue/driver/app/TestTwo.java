b2acbd2bc08c31a075ac27f14908feca0ab6eef4
Sharif Hossain <hossain.udhs@gmail.com>
2017-01-31 18:48:58 -0500

    Testing audio alert playback.
    -Sharif and Jason

--- app/src/main/java/com/fatigue/driver/app/TestTwo.java
+++ app/src/main/java/com/fatigue/driver/app/TestTwo.java
@@ -6,6 +6,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+import android.media.MediaPlayer;
 
 public class TestTwo extends Activity {
 
@@ -15,6 +16,9 @@ public class TestTwo extends Activity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         setContentView(R.layout.test_two);
+
+        MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.Loud_Alarm_Clock_Buzzer);
+        mediaPlayer.start();
 
 
     }
