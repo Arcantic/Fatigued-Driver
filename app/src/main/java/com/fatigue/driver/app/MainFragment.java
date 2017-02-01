d2c0abed50696cf896a094005ff221aeb762471b
Eric <esrex28@gmail.com>
2017-02-01 01:02:07 -0500

    Forgot to push some things...

--- app/src/main/java/com/fatigue/driver/app/MainFragment.java
+++ app/src/main/java/com/fatigue/driver/app/MainFragment.java
@@ -2,9 +2,15 @@ package com.fatigue.driver.app;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 /**
  * Created by Eric on 11/14/2016.
@@ -15,6 +21,30 @@ public class MainFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        RelativeLayout card = (RelativeLayout) view.findViewById(R.id.connection_status_layout);
+        DisplayMetrics displaymetrics = new DisplayMetrics();
+        getActivity().getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
+        int width = displaymetrics.widthPixels;
+        int half_width = (width
+                - (int)getResources().getDimension(R.dimen.card_default_padding)*2
+                - (int)getResources().getDimension(R.dimen.activity_horizontal_margin_cards)*2
+                - (int)getResources().getDimension(R.dimen.card_corners)*2)
+                /2;
+
+        RelativeLayout lay1 = (RelativeLayout) view.findViewById(R.id.connection_quality_layout);
+        RelativeLayout lay2 = (RelativeLayout) view.findViewById(R.id.battery_status_layout);
+        lay1.getLayoutParams().width = half_width;
+        lay2.getLayoutParams().width = half_width;
+
+        return view;
     }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
 }
