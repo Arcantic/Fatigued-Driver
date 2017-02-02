b8e56ee443a39ed39201b48e4d3c9b8954879994
Sharif Hossain <hossain.udhs@gmail.com>
2017-02-02 17:07:54 -0500

    Testing audio alert playback. Continued worked on TestTwo, added more code for Start/Stop.
    -Sharif and Jason

--- app/src/main/java/com/fatigue/driver/app/TestTwo.java
+++ app/src/main/java/com/fatigue/driver/app/TestTwo.java
@@ -4,11 +4,15 @@ package com.fatigue.driver.app; /*
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.media.MediaPlayer;
+import android.widget.Button;
 
 public class TestTwo extends Activity {
+
+    private Button btnStartStop =null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,12 +21,20 @@ public class TestTwo extends Activity {
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         setContentView(R.layout.test_two);
 
+        btnStartStop = (Button) findViewById(R.id.btn_alarm_start_stop);
+        final MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.alarm_alert);
+        btnStartStop.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
 
-
-
-        MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.alarm_alert);
-        mediaPlayer.start();
-
-
+                if (mediaPlayer.isPlaying()){
+                    mediaPlayer.stop();
+                    btnStartStop.setText("Start");
+                }
+                else {
+                    mediaPlayer.start();
+                    btnStartStop.setText("Stop");
+                }
+            }
+        });
     }
 }
