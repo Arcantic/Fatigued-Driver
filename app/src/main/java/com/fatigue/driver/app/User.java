089ab4206e38985af78410ced121b2a8173ec859
Sharif Hossain <hossain.udhs@gmail.com>
2017-02-07 20:53:34 -0500

    Wrote code for user integration.
    -Sharif and Russ

--- app/src/main/java/com/fatigue/driver/app/User.java
+++ app/src/main/java/com/fatigue/driver/app/User.java
@@ -1,13 +1,14 @@
 package com.fatigue.driver.app;
 
 import android.content.Context;
-import android.os.Environment;
-import android.util.Log;
 
+import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileOutputStream;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.ArrayList;
 
 /**
  * Created by Eric on 2/4/2017.
@@ -22,19 +23,30 @@ public class User {
     public static String user_name;
 
 
-
-    public static void changeUser(String user_name){
-        loadUser(user_name);
-    }
-
-    public static void loadUser(String user_name){
+    /*
+    public static void loadUser(String user_name, Context context) {
         //TODO: Load save file information...
         String loaded_user_name = user_name;
+
+        try {
+            FileInputStream fis = context.openFileInput(user_name+".txt");
+            InputStreamReader isr = new InputStreamReader(fis);
+            BufferedReader bufferedReader = new BufferedReader(isr);
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                sb.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         User.user_name = loaded_user_name;
     }
 
-    //Save user_name and current timestamp to internal file.
+
+        //Save user_name and current timestamp to internal file.
+
     public static void newUser(String user_name, Context context){
         //TODO: Add user to UI selection...
         String filename = user_name;
@@ -60,6 +72,49 @@ public class User {
             Log.e("Exception", "File write failed: " + e.toString());
         }
 
+
+        User.user_name = user_name;
+    }
+    */
+
+
+    public static void selectUser(String user_name){
+        User.user_name = user_name;
+    }
+
+    public static ArrayList loadUserList(Context context) {
+        ArrayList<String> user_list = new ArrayList<>();
+
+        try {
+            FileInputStream fis = context.openFileInput(manifest_file);
+            InputStreamReader isr = new InputStreamReader(fis);
+            BufferedReader bufferedReader = new BufferedReader(isr);
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                user_list.add(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return user_list;
+    }
+
+
+    public static String manifest_file = "user_list.txt";
+    public static void newUser(String user_name, Context context){
+        File _list = new File(manifest_file);
+        try{
+            if(_list.exists()==false){
+               //Make a new file
+                _list.createNewFile();
+            }
+            PrintWriter out = new PrintWriter(_list);
+            out.append(user_name+"\n");
+            out.close();
+        }catch(IOException e) {
+            System.out.println("COULD NOT LOG!!");
+        }
 
         User.user_name = user_name;
     }
