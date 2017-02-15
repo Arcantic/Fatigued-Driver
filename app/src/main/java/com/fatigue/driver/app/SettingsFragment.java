df910a9a7805d566e7c89480a4b54d93890af0ee
Arcantic <electric16>
2017-02-14 19:36:45 -0500

    Began adding and theming the Settings page.
    @Rus and Eric

--- app/src/main/java/com/fatigue/driver/app/SettingsFragment.java
+++ app/src/main/java/com/fatigue/driver/app/SettingsFragment.java
@@ -2,6 +2,7 @@ package com.fatigue.driver.app;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.preference.PreferenceFragmentCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,11 +10,11 @@ import android.view.ViewGroup;
 /**
  * Created by Eric on 11/15/2016.
  */
-public class SettingsFragment extends Fragment {
+public class SettingsFragment extends PreferenceFragmentCompat {
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        // Load the Preferences from the XML file
+        addPreferencesFromResource(R.xml.app_preferences);
     }
 }
