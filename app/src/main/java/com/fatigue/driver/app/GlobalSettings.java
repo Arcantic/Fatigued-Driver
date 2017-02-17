dafdcfe5609c55fa6de6f23561bdcf35afd0e6a3
jsnieves <jsnieves@mail.widener.edu>
2017-02-16 19:59:45 -0500

    Added additional (Global Settings) fields.
    -jsnieves

--- app/src/main/java/com/fatigue/driver/app/GlobalSettings.java
+++ app/src/main/java/com/fatigue/driver/app/GlobalSettings.java
@@ -1,15 +1,19 @@
 package com.fatigue.driver.app;
 
 public class GlobalSettings {
-    public static final String appRootFolder = "Fatigue";
+    public static final String appRootFolder = "DriverFatigue";
+    public static final String appSettingsFilename = "Config.ini";
+
     public static int samplingSizeInt = 512;
     public static int debugLevel = 5;
+
+    public static boolean verboseDebug = true;
 
     public GlobalSettings() {
     }
 
     public static String getAppRootFolderName() {
-        return "Fatigue";
+        return appRootFolder;
     }
 
     public static double getPollingIntervalInSeconds() {
