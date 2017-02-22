880129defa1453013d2fad878c0d0b2b3cbb39ed
jsnieves <jsnieves@mail.widener.edu>
2017-02-21 21:14:47 -0500

    Added additional / updated existing (GlobalSettings) class static values.
    -jsnieves

--- app/src/main/java/com/fatigue/driver/app/GlobalSettings.java
+++ app/src/main/java/com/fatigue/driver/app/GlobalSettings.java
@@ -1,22 +1,36 @@
 package com.fatigue.driver.app;
 
 public class GlobalSettings {
-    public static final String appRootFolder = "DriverFatigue";
-    public static final String appSettingsFilename = "Config.ini";
+    public static final String APP_SETTINGS_FILENAME = "config.ini";
+    public static final int COLLECTION_INTERVAL_DURATION_ALERT_IN_SECONDS = 5;
+    public static String appRootFolderName = "DriverFatigue";
+    public static String rawFolderName = "RawData";
+    public static String rawLogFileName = "Raw.log";
+    public static String rawNormLogFileName = "RawNorm.log";
+    public static String magLogFileName = "Magnitude.log";
+    public static String magAvgLogFileName = "MagnitudesAveraged.log";
+    public static String fftComplexLogFileName = "FFT.log";
+    public static String rawComplexLogFileName = "RawComplex.log";
+    public static boolean isDebug = true;
+    public static boolean isDebugVerbose = true;
+    public static int samplingSizeInterval = 512;
+    public static int collectionIntervalDurationFatigue = 2;
+    public static int calibrationTotalNumOfTrialsToPerformAlert = 100;
+    public static int calibrationTotalNumOfTrialsToPerformFatigue = 100;
 
-    public static int samplingSizeInt = 512;
-    public static int debugLevel = 5;
-
-    public static boolean verboseDebug = true;
 
     public GlobalSettings() {
     }
 
     public static String getAppRootFolderName() {
-        return appRootFolder;
+        return appRootFolderName;
+    }
+
+    public static void setAppRootFolderName(String s){
+        appRootFolderName = s;
     }
 
     public static double getPollingIntervalInSeconds() {
-        return (double) (samplingSizeInt / 512);
+        return (double) (samplingSizeInterval / 512);
     }
 }
