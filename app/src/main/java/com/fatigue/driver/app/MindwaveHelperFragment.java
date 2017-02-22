c87cfafed888946c90604389f3fff03982b6bda2
jsnieves <jsnieves@mail.widener.edu>
2017-02-21 21:12:09 -0500

    Updated (MindwaveHelperFragment) class.
    -jsnieves

--- app/src/main/java/com/fatigue/driver/app/MindwaveHelperFragment.java
+++ app/src/main/java/com/fatigue/driver/app/MindwaveHelperFragment.java
@@ -1,42 +1,60 @@
 package com.fatigue.driver.app;
 
+import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.os.Bundle;
-import android.os.Handler;
+import android.os.Environment;
 import android.os.Message;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.widget.Toast;
 
 import com.neurosky.connection.ConnectionStates;
-import com.neurosky.connection.DataType.MindDataType;
-import com.neurosky.connection.EEGPower;
 import com.neurosky.connection.TgStreamHandler;
 import com.neurosky.connection.TgStreamReader;
 
 public class MindwaveHelperFragment extends android.support.v4.app.Fragment {
 
     private static final String TAG = MindwaveHelperFragment.class.getSimpleName();
+
+    //private boolean isDebugVerbose = true;  //jsnieves:migrated to GlobalSettings
+    //private boolean isDebug = true;
+
     private static final int REQUEST_WRITE_STORAGE = 112;
     private static final int MSG_UPDATE_BAD_PACKET = 1001;
     private static final int MSG_UPDATE_STATE = 1002;
-    public BluetoothAdapter mBluetoothAdapter;
-    //private Complex[] fftComplexArrayResults;
-    public TgStreamReader tgStreamReader;
-    private TgStreamHandler callback;
-    private Handler LinkDetectedHandler;
-    private Complex[] rawComplexArray;
+    boolean isRecordingAlert;
+    boolean isRecordingFatigue = !isRecordingAlert;
+    boolean isRecordingRawData;
+    boolean isRecordingRawNormData;
+    boolean isCalcFFT;
+
+    //boolean isRecording=true; //see TgStreamReader volatile
+    boolean isRecordingFFT;
+    boolean isRecordingMagnitudeArray;
+    boolean isRecordingMagnitudeAveragedArray;
     private int badPacketCount;
+    private BluetoothAdapter mBluetoothAdapter;
+    private TgStreamReader tgStreamReader;
+    private TgStreamHandler callback;
+    private LinkDetectedHandler LinkDetectedHandler;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        initConnection();
-        System.out.println("##############");
-        System.out.println("initConnection");
-        System.out.println("##############");
+        boolean hasWritePermission = ContextCompat.checkSelfPermission(getActivity(), "android.permission.WRITE_EXTERNAL_STORAGE") == 0;
+        if (!hasWritePermission) {
+            ActivityCompat.requestPermissions(getActivity(), new String[]{"android.permission.WRITE_EXTERNAL_STORAGE"}, 112);
+        }
+        Activity parentActivity = getActivity();
+        System.out.println(parentActivity.toString());
+        //this.initLogCreate();
+        LinkDetectedHandler = new LinkDetectedHandler(getContext());
+        this.initConnection();
+        //System.out.println("##initConnection##");
     }
-
 
     public boolean initConnection() {
 
@@ -53,13 +71,11 @@ public class MindwaveHelperFragment extends android.support.v4.app.Fragment {
                 return false;
             }
 
-
             if (mBluetoothAdapter.isEnabled()) {
                 Toast.makeText(getActivity(), "Bluetooth enabled.", Toast.LENGTH_LONG).show(); //finish();
             }
 
-
-            LinkDetectedHandler = this.initLinkDetectedHandler();
+            //LinkDetectedHandler = this.initLinkDetectedHandler();
             callback = this.initCallback();
 
             // Example of constructor public TgStreamReader(BluetoothAdapter ba, TgStreamHandler tgStreamHandler)
@@ -69,8 +85,6 @@ public class MindwaveHelperFragment extends android.support.v4.app.Fragment {
             tgStreamReader = new TgStreamReader(mBluetoothAdapter, callback);
             //tgStreamReader.setGetDataTimeOutTime(6);
             //tgStreamReader.startLog();
-
-
 
             badPacketCount = 0;
 
@@ -87,20 +101,17 @@ public class MindwaveHelperFragment extends android.support.v4.app.Fragment {
             tgStreamReader.connect();
             // tgStreamReader.connectAndStart();
 
-
-
         } catch (Exception e) {
             e.printStackTrace();
             Log.i(TAG, "error:" + e.getMessage());
             return false;
         }
 
-    return true;
+        return true;
 
     }
 
-
-    private TgStreamHandler initCallback() {
+    private TgStreamHandler initCallback() { //TODO:migrate to Modified?
         return new TgStreamHandler() {
 
             @Override
@@ -110,11 +121,11 @@ public class MindwaveHelperFragment extends android.support.v4.app.Fragment {
                 switch (connectionStates) {
 
                     case ConnectionStates.STATE_INIT:
-                        if (GlobalSettings.verboseDebug) showToast("STATE_INIT", Toast.LENGTH_SHORT);
+                        if (GlobalSettings.isDebugVerbose) showToast("STATE_INIT", Toast.LENGTH_SHORT);
                         break;
 
                     case ConnectionStates.STATE_CONNECTING:
-                        if (GlobalSettings.verboseDebug) showToast("STATE_CONNECTING", Toast.LENGTH_SHORT);
+                        if (GlobalSettings.isDebugVerbose) showToast("STATE_CONNECTING", Toast.LENGTH_SHORT);
                         break;
 
                     case ConnectionStates.STATE_CONNECTED:
@@ -126,8 +137,12 @@ public class MindwaveHelperFragment extends android.support.v4.app.Fragment {
                         //(9) demo of recording raw data , stop() will call stopRecordRawData,
                         //or you can add a button to control it.
                         //You can change the save path by calling setRecordStreamFilePath(String filePath) before startRecordRawData
+
+                        //jsnieves:COMMENT:needed anymore??
                         tgStreamReader.startRecordRawData();
-                        if (GlobalSettings.verboseDebug) showToast("STATE_WORKING", Toast.LENGTH_SHORT);
+                        //??
+
+                        if (GlobalSettings.isDebugVerbose) showToast("STATE_WORKING", Toast.LENGTH_SHORT);
                         break;
 
                     case ConnectionStates.STATE_STOPPED:
@@ -209,173 +224,100 @@ public class MindwaveHelperFragment extends android.support.v4.app.Fragment {
         };
     }
 
-    private Handler initLinkDetectedHandler() {
-        return new Handler() {
-            int count = 0;
-            double rawReal;
-            double rawImaginary;
-            Complex[] rawComplexArray = new Complex[512];
-            double[] rawRealArray= new double[rawComplexArray.length];
-            double[] rawRealNormalizedArray = new double[rawComplexArray.length];
-            double magnitudeArray[] = new double[rawComplexArray.length];
+    private LinkDetectedHandler initLinkDetectedHandler() {
+        return new LinkDetectedHandler(getContext());
+    }
 
+    @Override
+    public void onStop() {
+        tgStreamReader.stop();
+        tgStreamReader.close();
+        super.onStop();
 
-            @Override
-            public void handleMessage(Message msg) {
-                // jsnieves:COMMENT:Handles various MindDataTypes
-                switch (msg.what) {
+    }
 
-                    case MindDataType.CODE_MEDITATION:
-                        Log.d(TAG, "HeadDataType.CODE_MEDITATION " + msg.arg1);
-                        //tv_meditation.setText("" + msg.arg1);
-                        break;
-
-                    case MindDataType.CODE_ATTENTION:
-                        Log.d(TAG, "CODE_ATTENTION " + msg.arg1);
-                        //tv_attention.setText("" + msg.arg1);
-                        break;
-
-                    case 3: //jsnieves:COMMENT:0x03 HEART_RATE (0-255) 1Hz on EGO.
-                        Log.d(TAG, "CODE_HEARTRATE " + msg.arg1);
-                        //tv_heartrate.setText("" + msg.arg1);
-                        break;
-                /* jsnieves:COMMENT:unfortunately, this is not a native feature of our headset model.
-                   "http://developer.neurosky.com/docs/doku.php?id=thinkgear_communications_protocol"
-                   TODO:revisit and consider manual implementations */
-
-                    case MindDataType.CODE_POOR_SIGNAL:
-                        int poorSignal = msg.arg1;
-                        //if (isDebugVerbose || isDebug) Log.d(TAG, "poorSignal:" + poorSignal);
-                        //tv_ps.setText("" + msg.arg1);
-                        break;
-
-                    case MindDataType.CODE_FILTER_TYPE: //jsnieves:COMMENT:Enum FilterType : FILTER_50HZ(4), FILTER_60HZ(5)
-                        Log.d(TAG, "CODE_FILTER_TYPE: " + msg.arg1);
-                        break;
-
-                    case MSG_UPDATE_BAD_PACKET:
-                        //tv_badpacket.setText("" + msg.arg1);
-                        break;
-
-                    case MindDataType.CODE_RAW:
-                        //jsnieves:COMMENT:Logs
-                        //Log.i(TAG, "Raw " + msg.arg1);
-                        //jsnieves:COMMENT:Graph update
-                        //updateWaveView(msg.arg1);
-
-                        rawReal = (double) msg.arg1;
-                        rawImaginary = 0.0;
-                        System.out.println("msg.arg1: " + msg.arg1);
-                        System.out.println("rawReal: " + rawReal);
-
-                        //Complex temprawComplex = new Complex(rawReal, rawImaginary);
-
-                        //rawComplexArray[count] = new Complex(rawReal, rawImaginary);
-                        rawRealArray[count] = rawReal;
-
-                        //System.out.println("rawComplexArray" + "["+ count + "]" + rawComplexArray[count].re() + rawComplexArray[count].im());
-
-
-                        count++;
-
-                        //==
-                        if (count >= 512) {
-
-                            count = 0;
-                            //Complex[] temp = rawComplexArray;
-                            //jsnieves:COMMENT:compute FFT, store results in fftComplexArrayResults
-                            //fftComplexArrayResults = FFT.fft(rawComplexArray);
-
-                            //fftComplexArrayResults = new Complex[512];
-
-
-                            //TODO: SD HERE
-
-                            double sd = Util.calcSD(rawRealArray);
-                            double mean = Util.calcMean(rawRealArray);
-                            for(int i =0; i< rawRealArray.length ; i++){
-                                rawRealNormalizedArray[i] = (rawRealArray[i] - mean)/ sd;
-                                rawComplexArray[i] = new Complex(rawRealNormalizedArray[i], rawImaginary);
-                            }
-                            Complex[] fftComplexArrayResults = FFT.fft(rawComplexArray);
-                            //caleb magnitude testing
-                            magnitudeArray = Magnitude.mag(fftComplexArrayResults);
-                            //double normalization[] = Magnitude.norm(magnitudeArray);
-                            //printing output of magnitude array
-                            for (int i = 0; i < magnitudeArray.length; i++) {
-                                System.out.println("MAGNITUDE" + "[" + i + "]: " + magnitudeArray[i]);
-                            }
-
-
-                            rawComplexArray = new Complex[512];
-                        }
-                        break;
-
-                    case MindDataType.CODE_EEGPOWER:
-                        EEGPower power = (EEGPower) msg.obj;
-                        if (power.isValidate()) {
-
-
-                            /*
-
-                            //jsnieves:BEGIN:simple conversions dB
-                            //(rawValue * (1.8/4096)) / 2000 ... convert TGAT-based EEG sensor values (such as TGAT, TGAM, MindWave, MindWave Mobile) to voltage values
-                            //TODO:Test/complete
-                            //jsnieves:END:simple conversions
-
-
-                            //jsnieves:BEGIN:Log
-                            //TODO:retag as RAW Power
-                            //jsnieves:COMMENT:Gamma (low and middle)
-
-                            //Log.i(TAG, "lowGammaRaw " + power.lowGamma);
-                            //Log.i(TAG, "lowGammaVolts " + powerToVolts(power.lowGamma));
-                            //Log.i(TAG, "middleGammaRaw " + power.middleGamma);
-                            //Log.i(TAG, "middleGammaVolts " + powerToVolts(power.middleGamma));
-
-                            //jsnieves:COMMENT:Beta (low and high)
-                            //Log.i(TAG, "lowBetaRaw " + power.lowBeta);
-                            //Log.i(TAG, "lowBetaVolts " + powerToVolts(power.lowBeta));
-                            //Log.i(TAG, "highBetaRaw " + power.highBeta);
-                            //Log.i(TAG, "highBetaVolts " + powerToVolts(power.highBeta));
-
-                            //jsnieves:COMMENT:Alpha (low and high)
-                            Log.i(TAG, "lowAlphaRaw " + power.lowAlpha);
-                            Log.i(TAG, "lowAlphaVolts " + powerToVolts(power.lowAlpha));
-                            Log.i(TAG, "highAlphaRaw " + power.highAlpha);
-                            Log.i(TAG, "highAlphaVolts " + powerToVolts(power.highAlpha));
-
-                            //jsnieves:COMMENT:Theta
-
-                            //Log.i(TAG, "ThetaRaw " + power.theta);
-                            //Log.i(TAG, "ThetaVolts " + powerToVolts(power.theta));
-
-                            //jsnieves:COMMENT:Delta
-                            //Log.i(TAG, "DeltaRaw " + power.delta);
-                            //Log.i(TAG, "DeltaVolts " + powerToVolts(power.delta));
-
-                            //jsnieves:END:Log
-
-                            */
-
-                            //TODO perhaps store power.delta to a value (as it is in constant flux)
-                            //TODO: set lowest to "gone" in layout, due to information being mostly irrelevant and almost always ending up at ZERO
-                            //TODO: check to make sure textView contains a number and not a String
-
-                        }
-                        break;
-
-                    default:
-                        //jsnieves:COMMENT:Handle and Log any/all other messages
-                        //Log.d(TAG, "UNKNOWN DataType Result: " + msg.arg1);
-                        break;
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 112:
+                if (grantResults.length <= 0 || grantResults[0] != 0) {
+                    Toast.makeText(getActivity().getParent(), "Write access denied. App functionality may be limited. Please consider granting this permission.", Toast.LENGTH_SHORT).show();
                 }
-                super.handleMessage(msg);
-            }
-        };
+            default:
+        }
+    }
+
+    //jsnieves:migrate to Util
+    public boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        return "mounted".equals(state);
+    }
+
+    public boolean isExternalStorageReadable() {
+        String state = Environment.getExternalStorageState();
+        return "mounted".equals(state) || "mounted_ro".equals(state);
     }
 
 
+    public double[] normalizeRawArray(final double[] rawArray){
+
+        double[] normArray = new double[rawArray.length];
+
+        double sd = Util.calcSD(rawArray);
+        double mean = Util.calcMean(rawArray);
+        for(int i =0; i< rawArray.length ; i++){
+            normArray[i] = (rawArray[i] - mean)/ sd;
+            //rawComplexArray[i] = new Complex(rawRealNormalizedArray[i], rawImaginary);
+        }
+
+        return normArray;
+    }
+
+
+
+
+    public Complex[] makeComplexArray(final double[] rawNormArray){
+        Complex[] complexArray=new Complex[rawNormArray.length];
+
+        double imaginary = 0.0;
+
+        for(int i=0 ; i< rawNormArray.length; i++){
+            complexArray[i]=new Complex(rawNormArray[i], imaginary);
+        }
+
+        return complexArray;
+    }
+
+
+    public void parseMagnitudeArray(){
+
+        //divide by bands here
+        //log
+        //average
+        //log
+    }
+
+
+    public void startRecordAll(){
+        //isRecording = true;
+        setRecordingRawData(true);
+        //set
+        //set
+        //set
+    }
+
+    public void setRecordingRawData(boolean b){
+        this.isRecordingRawData = b;
+    }
+
+    public void stopRecordAll(){
+        isRecordingRawData = false;
+        isRecordingRawNormData = false;
+        isRecordingFFT = false;
+        isRecordingMagnitudeArray = false;
+        isRecordingMagnitudeAveragedArray = false;
+        //  isRecording = false;
+    }
 
 
     public void showToast(final String msg, final int timeStyle) {
@@ -386,6 +328,7 @@ public class MindwaveHelperFragment extends android.support.v4.app.Fragment {
     }
 
 }
+
 
 
 
