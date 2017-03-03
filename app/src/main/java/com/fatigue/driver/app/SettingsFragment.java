bfb931f890aee303bf89fb4b821708d77b4d3c40
jsnieves <jsnieves>
2017-03-03 00:01:16 -0500

    Commented out just about (EVERYTHING) in [SettingsFragment] class.
    -jsnieves

--- app/src/main/java/com/fatigue/driver/app/SettingsFragment.java
+++ app/src/main/java/com/fatigue/driver/app/SettingsFragment.java
@@ -1,11 +1,7 @@
 package com.fatigue.driver.app;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v7.preference.PreferenceFragmentCompat;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 /**
  * Created by Eric on 11/15/2016.
@@ -18,72 +14,73 @@ public class SettingsFragment extends PreferenceFragmentCompat {
         addPreferencesFromResource(R.xml.app_preferences);
     }
 }
-public class DeviceListAdapter extends BaseAdapter {
-
-    private LayoutInflater mInflater;
-    private List<BluetoothDevice> mData;
-    private OnConnectButtonClickListener connectListener;
-
-    public DeviceListAdapter(Context context) {
-        mInflater = LayoutInflater.from(context);
-    }
-
-    public void setData(List<BluetoothDevice> data) {
-        mData = data;
-    }
-
-    public void setListener(OnConnectButtonClickListener listener) {
-        connectListener = listener;
-    }
-
-    public int getCount() {
-        return (mData == null) ? 0 : mData.size();
-    }
-
-    public Object getItem(int position) {
-        return null;
-    }
-
-    public long getItemId(int position) {
-        return position;
-    }
-
-    public View getView(final int position, View convertView, ViewGroup parent) {
-        ViewHolder holder;
-
-        if (bluetoothInfo.isConnected()) {
-            holder.connectBtn.setText("connected");
-        } else {
-            holder.connectBtn.setText("connect");
-        }
-
-        BluetoothDevice device = mData.get(position);
-
-        holder.nameTv.setText(device.getName());
-        holder.addressTv.setText(device.getAddress());
-        holder.connectBtn.setText("connect");
-        holder.connectBtn.setText("connected");
-
-        holder.connectBtn.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                if (connectListener != null) {
-                    connectListener.onConnectButtonClick(position);
-                }
-            }
-        });
-
-        return convertView;
-    }
-
-    static class ViewHolder {
-        TextView nameTv;
-        TextView addressTv;
-        Button connectBtn;
-    }
-
-    public interface OnConnectButtonClickListener {
-        public abstract void onConnectButtonClick(int position);
-
-    }
+//public class DeviceListAdapter extends BaseAdapter {
+//
+//    private LayoutInflater mInflater;
+//    private List<BluetoothDevice> mData;
+//    private OnConnectButtonClickListener connectListener;
+//
+//    public DeviceListAdapter(Context context) {
+//        mInflater = LayoutInflater.from(context);
+//    }
+//
+//    public void setData(List<BluetoothDevice> data) {
+//        mData = data;
+//    }
+//
+//    public void setListener(OnConnectButtonClickListener listener) {
+//        connectListener = listener;
+//    }
+//
+//    public int getCount() {
+//        return (mData == null) ? 0 : mData.size();
+//    }
+//
+//    public Object getItem(int position) {
+//        return null;
+//    }
+//
+//    public long getItemId(int position) {
+//        return position;
+//    }
+//
+//    public View getView(final int position, View convertView, ViewGroup parent) {
+//        ViewHolder holder;
+//
+//        if (bluetoothInfo.isConnected()) {
+//            holder.connectBtn.setText("connected");
+//        } else {
+//            holder.connectBtn.setText("connect");
+//        }
+//
+//        BluetoothDevice device = mData.get(position);
+//
+//        holder.nameTv.setText(device.getName());
+//        holder.addressTv.setText(device.getAddress());
+//        holder.connectBtn.setText("connect");
+//        holder.connectBtn.setText("connected");
+//
+//        holder.connectBtn.setOnClickListener(new View.OnClickListener() {
+//
+//            @Override
+//            public void onClick(View v) {
+//                if (connectListener != null) {
+//                    connectListener.onConnectButtonClick(position);
+//                }
+//            }
+//        });
+//
+//        return convertView;
+//    }
+//
+//    static class ViewHolder {
+//        TextView nameTv;
+//        TextView addressTv;
+//        Button connectBtn;
+//    }
+//
+//    public interface OnConnectButtonClickListener {
+//        public abstract void onConnectButtonClick(int position);
+//
+//    }
+//
